a5ae5a22fd91cefaf5d9198c94c2ead6e4521277
khananas4513 <khananas4513@gmail.com>
2026-07-10 02:03:57 +0530

    Add Two Pointers solution for Trapping Rain Water

---
+++ Arrays/0042-Trapping-Rain-Water/TwoPointers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int n = height.length;
+
+        int water = 0;
+
+        int left = 0;
+        int right = n - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        while (left < right) {
+
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+
+            if (leftMax < rightMax) {
+                water += leftMax - height[left];
+                left++;
+            } else {
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
